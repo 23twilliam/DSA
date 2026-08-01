827feb140e40b3ca0c108e17bcc9696fb6c37b6e
23twilliam <172639957+23twilliam@users.noreply.github.com>
2026-08-01 19:01:31 +0100

    Easy 2 sum

---
+++ 22_07_2026/1_Two_Sum.py
@@ -0,0 +1,39 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#
+# Example 2:
+#
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+#
+# Example 3:
+#
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range (len(nums)):
+            pair = target - nums[i]
+            if pair in hashmap: # check if we already saw the number we need
+                return [i, hashmap[pair]]
+            hashmap[nums[i]] = i # if not add the current number to the map
+        return []
+
+sol = Solution()
+print(sol.twoSum([2, 4, 3], 6))
